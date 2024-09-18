db993ef110d9482b05b8c5a9d36561ad5be171e5
CityWasTaken <smithfaheed@gmail.com>
2024-09-18 13:03:34 -0400

    Finished array basics

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 // Arrays are primitive values
 // They are reference types
-const fruits = ["apple", "orange", "kiwi"];
-const names = ["Roman", "Michael", "Nina", "Adonis"];
-const data = ["JD", "Tadlock", 44, "555 coding dr", "777-777-7777"]
+const fruits = ["apple", "orange", "kiwi", "grape"];
+const names = ["Roman", "Michael", "Nina", "Adonis", "JD"];
+const data = ["JD", "Tadlock", 44, "555 coding dr", "777-777-7777", ["pickleball", "fishing"]];
 
-console.log(fruits.length);
-console.log(names[1]);
+console.log(fruits);
+
+fruits[2] = "strawberry";
+
+console.log(fruits);
+
+// increase the data age number by one - console.log the original and new value after you increase it
+
+console.log(data[2]);
+
+data[2] ++;
+
+console.log(data[2]);
+
+
