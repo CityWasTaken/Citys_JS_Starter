06069c13115d54f8b741edda80f62dfa26ec3aef
CityWasTaken <smithfaheed@gmail.com>
2024-09-18 11:55:42 -0400

    Finished conditional overview

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 const firstName = "City";
-let age = 27;
+let age = 12;
 
-// age = age + 1;
-// age += 1;
-// age++; /only increases by 1
+// if age is greater than 20 and less than 50, console log "you are young"
 
-// age = age - 1;
-// age -= 1;
-// age--; 
+// if age is greater than or equal to 50, console log "you are getting older"
 
-console.log(age);
+// if age is less than 20 and greater than 12, console log "you are a teenager"
 
+if (age > 20 && age < 50 ) {
+    console.log("you are young");
+ } else if (age >= 50 ) {
+     console.log("you are getting older");
+ } else if (age < 20 && age > 12) {
+        console.log("you are a teenager");
+ }
+
+ else {
+    console.log("you are a child")
+ }
