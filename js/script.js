9c4d263b28bba671e14ae15f953e2a649bf734c8
CityWasTaken <smithfaheed@gmail.com>
2024-09-18 12:17:41 -0400

    Created examples to show how arrays work

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Arrays are primitive values
+// They are reference types
+const fruits = ["apple", "orange", "kiwi"];
+const names = ["Roman", "Michael", "Nina", "Adonis"];
+const data = ["JD", "Tadlock", 44, "555 coding dr", "777-777-7777"]
+
+console.log(fruits.length);
+console.log(names[1]);
