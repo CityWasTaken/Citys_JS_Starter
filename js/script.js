8a948c8a8abbfe0128cb77ba774b73cccdf9b359
CityWasTaken <smithfaheed@gmail.com>
2024-09-20 14:30:52 -0400

    Started working with web api

--- js/script.js
+++ js/script.js
@@ -1,40 +1,51 @@
-let userData = {
-    name: "City",
-    age: 27,
-    phone: "322-332-3232",
-    adress: "32 coding ave",
-    info: {
-        location: "Jersey area",
-        hobbies: ["Music", "Gaming"]
-    },
-    printAge: function () {
-        console.log(this.age);
-    },
-    haveBirthday: function() {
-        // increase age by one
-        this.age ++;
-        // print "Happy Birthday" tp the console
-        console.log("Happy Birthday");
-    },
-    addHobby: function(hobby) {
-        // Need to reference and arguement that is passed to addHobby (string of a hobby)
+const header = document.querySelector("h1");
+const paragraph = document.querySelector(".special");
+const image = document.querySelector("#main-image");
+const noteOutput = document.querySelector(".output");
+const timerDisplay = document.querySelector("#time")
+const buttonTimer = document.querySelector("#start-button")
 
-        // push the arguement(hobby) to the info.hobbies array
-        this.info.hobbies.push(hobby);
-    },
-    printHobbies: function() {
-        // console.log the hobbies array
-        console.log(this.info.hobbies);
-        // BONES - loop over the hobbies array and console.log each hobby
-        for (let hobby of this.info.hobbies) {
-            console.log(hobby);
-        }
+header.classList.add("crazy");
+
+header.style.textDecoration = "underline";
+
+// image.setAttribute("src", "https://images.pexels.com/photos/27700804/pexels-photo-27700804/free-photo-of-a-man-is-riding-a-boat-through-a-tunnel.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+
+// Insert a new article element into the output section
+
+noteOutput.insertAdjacentHTML("beforeend", `
+    <article>
+        <h3>new note text</h3>
+        <p>Added On: 09-20-2024<p>
+    </article>
+    `);
+
+// Timers
+
+// window.setTimeout(function () {
+//     console.log("time up!");
+// }, 3000);
+
+let count = 10;
+let started = false
+
+
+buttonTimer.addEventListener("click", function() {
+    if (!started) {
+        const timer = setInterval(function () {
+            count--;
+        
+            timerDisplay.innerText = "Time: " + count;    
+        
+            if (count <= 0) {
+                clearInterval(timer);     
+            
+            timerDisplay.innerText = "Time: 10";
+                count = 10;
+                started = false;
+            }
+        }, 500);
+
+        started = true;
     }
-};
-
-userData.printHobbies();
-userData.printHobbies();
-userData.addHobby("golf");
-userData.addHobby("golf");
-userData.addHobby("golf");
-userData.printHobbies();
+});
